bd615c171626554fbb0735f84c7d66077ead32ff
Yashraj Bains <yashraj@sdfrome001.sdf.slac.stanford.edu>
2024-07-30 15:17:47 -0700

    Renaming directories/files. Setting up tests

---
+++ tests/test_pz_Evaluation.py
@@ -0,0 +1,8 @@
+import pytest
+import pz_Evaluation as yf
+
+def test_algorithm_flavor():
+    assert yf.algorithm_flavor('fzb_base_base') == 'fzboost'
+    assert yf.algorithm_flavor('knn_base_HSC') == 'knn'
+    assert yf.algorithm_flavor('a_b_c') == 'a'
+
